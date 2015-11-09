ad8dfa0da3b9b6eb90c96fa24623a2a8df380bcb
Christopher Heriot <cheriot@gmail.com>
2015-11-09 16:14:57 +0700

    Add routes for home and location pages.

---
+++ react/components/place_home.js
@@ -0,0 +1,11 @@
+import React from 'react/addons';
+
+class PlaceHome extends React.Component {
+
+  render() {
+    return <div>Place Home page</div>
+  }
+
+}
+
+module.exports = PlaceHome
--- react/routes.js
+++ react/routes.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Route, DefaultRoute } from 'react-router';
+
 import Root from './components/root';
+import Home from './components/home';
+import PlaceHome from './components/place_home';
+
 import ManagementRoot from './components/management_root';
 import FeedManagement from './components/feed_management';
 import FeedManagementEditor from './components/feed_management_editor';
@@ -8,10 +12,13 @@ import EntryQueue from './components/entry_queue';
 
 var routes = (
   <Route path="/" handler={Root}>
+    <DefaultRoute handler={Home} />
+    <Route path="location/:id" handler={PlaceHome} />
+
     <Route path="management" handler={ManagementRoot}>
-      <Route path="feeds" handler={FeedManagement}/>
-      <Route path="feeds/:id" handler={FeedManagementEditor}/>
-      <Route path="queue" handler={EntryQueue}/>
+      <Route path="feeds" handler={FeedManagement} />
+      <Route path="feeds/:id" handler={FeedManagementEditor} />
+      <Route path="queue" handler={EntryQueue} />
     </Route>
   </Route>
 );

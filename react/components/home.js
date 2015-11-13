b325f6d7d4aa57cad9c92feaa8e56ec0e754a8f8
Christopher Heriot <cheriot@gmail.com>
2015-11-13 17:30:57 +0700

    Remove MapViewStore from Home to stop setState errors

--- react/components/home.js
+++ react/components/home.js
@@ -1,21 +1,11 @@
 import React from 'react';
 
 import MapViewActions from '../actions/map_view_actions';
-import MapViewStore from '../stores/map_view_store';
 import NavigationAutocomplete from './navigation_autocomplete';
 
 class Home extends React.Component {
 
-  componentDidMount() {
-    MapViewStore.listen(state => this.setState(state))
-  }
-
-  componentWillUnmount() {
-    MapViewStore.unlisten(this.handleChange);
-  }
-
   render() {
-
     return (
       <div>
         <NavigationAutocomplete />

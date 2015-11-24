3b171b8f13dd4dfa927ef3c4aeb7e97e9fe93c07
Christopher Heriot <cheriot@gmail.com>
2015-11-24 23:55:37 +0700

    Populate feedEntries with summary text and image.

---
+++ db/migrations/20151124142519_feed_entry_summary.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('feed_entries', t => {
+    t.string('summary_title');
+    t.string('summary_description');
+    t.binary('summary_thumbnail');
+    t.timestamp('summarized_at');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('feed_entries', t => {
+    t.dropColumn('summary_title');
+    t.dropColumn('summary_description');
+    t.dropColumn('summary_thumbnail');
+    t.dropColumn('summarized_at');
+  });
+};
--- server/models/etl/feed_entry_importer.js
+++ server/models/etl/feed_entry_importer.js
@@ -1,4 +1,7 @@
+import _ from 'lodash';
+import agent from '../../../react/sources/agent';
 import Feed from '../feed';
+import FeedEntry from '../feed_entry';
 import FeedReader from './feed_reader';
 import EntryResource from '../../resources/entry_resource';
 
@@ -24,4 +27,69 @@ export function importFeedEntries() {
 
       return Promise.all(feedPromises);
     })
+}
+
+function fetchSummary(uri) {
+  return agent
+    .get('http://link-thumbnailer-api.herokuapp.com/thumbnails/new')
+    .query({url: uri})
+    .then(res => res.body);
+}
+
+function generateThumbnail(summary) {
+  // {src, size[x,y], type}
+  const image = _.first(summary.images);
+  console.log('generateThumbnail of', image);
+  return agent
+    .get(image.src)
+    .then(res => res.body.toString('base64'))
+    .then(thumbnail => {
+      summary.thumbnail = thumbnail;
+      return summary;
+    });
+}
+
+function summarize(feedEntry) {
+  const uri = feedEntry.get('uri');
+  console.log('summarize', feedEntry.get('id'), feedEntry.get('title'), uri);
+  return fetchSummary(uri)
+    .then(generateThumbnail)
+    .then(summary => {
+      // maybe persist: feed.favicon, video thumbnail
+      feedEntry.set({
+        summary_title: summary.title,
+        summary_description: summary.description,
+        summary_thumbnail: summary.thumbnail,
+      });
+      return feedEntry.save();
+    })
+    .catch(err => {
+      console.log('ERROR', err);
+      console.log('continue..');
+      return Promise.all([]);
+      //throw err;
+    });
+}
+
+function promiseInSequence(array, func) {
+  const first = _.first(array);
+  const remaining = _.rest(array);
+  const promise = Promise.resolve(null).then(() => func(first));
+
+  return _.reduce(
+    remaining,
+    (p, element) => p.then(() => func(element)),
+    promise
+  );
+}
+
+export function summarizeFeedEntries(force=false) {
+  const all = FeedEntry.where('published_state', 'in', ['published', 'queued']);
+  const summarizable = force ? all : all.where({summarized_at: null})
+  return summarizable
+    .fetchAll()
+    .then(collection => collection.models)
+    .then(feedEntries => {
+      return promiseInSequence(feedEntries, summarize);
+    });
 }

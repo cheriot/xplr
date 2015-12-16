e87ac50880b9493a2a104821b32005c3a87a5f9f
Christopher Heriot <cheriot@gmail.com>
2015-12-16 10:24:20 +0700

    Task to only update summaries

---
+++ server/tasks/update_feed_entry_summaries.js
@@ -0,0 +1,7 @@
+import {summarizeFeedEntries} from '../models/etl/feed_entry_importer';
+
+summarizeFeedEntries()
+  .then(() => process.exit())
+  .catch((err) => {
+    console.log(`ERROR: ${err}`);
+  });

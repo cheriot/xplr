3af74dfe9ada3850b510b1b0c5d16c58d6fba08d
Christopher Heriot <cheriot@gmail.com>
2015-12-31 11:31:42 +0700

    Serialize feed processing for easier debugging

--- server/models/etl/feed_entry_importer.js
+++ server/models/etl/feed_entry_importer.js
@@ -15,23 +15,23 @@ export function importFeedEntries() {
     .query('orderBy', 'title')
     .fetchAll()
     .then((collection) => {
-      const feedPromises = collection.models.map((feed) => {
-        console.log(`Starting to update feed ${feed.get('title')} at ${feed.get('uri')}`);
-
-        return new FeedReader(feed.get('uri')).fetch()
-          .then((reader) => {
-            feed.updateOrInitializeFromRemote(reader.meta);
-            return Promise.all([reader, feed.save()]);
-          }).then(([reader, feed]) => {
-            const importPost = (post) => {
-              return EntryResource.updateOrCreate(feed, post);
-            };
-            return Promise.all(reader.posts.map(importPost));
-          });
-      });
-
-      return Promise.all(feedPromises);
+      return promiseInSequence(collection.models, fetchFeedEntries);
     })
+}
+
+function fetchFeedEntries(feed) {
+  console.log(`Starting to update feed ${feed.get('title')} at ${feed.get('uri')}`);
+
+  return new FeedReader(feed.get('uri')).fetch()
+    .then((reader) => {
+      feed.updateOrInitializeFromRemote(reader.meta);
+      return Promise.all([reader, feed.save()]);
+    }).then(([reader, feed]) => {
+      const importPost = (post) => {
+        return EntryResource.updateOrCreate(feed, post);
+      };
+      return promiseInSequence(reader.posts, importPost);
+    });
 }
 
 function fetchSummary(uri) {

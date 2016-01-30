d55ed64ce1fdb1cb4cffc94862fffc698ec788d7
Christopher Heriot <cheriot@gmail.com>
2016-01-30 15:43:16 +0700

    Allow concurrent web processes within a single heroku dyno.

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-// Which of these really needs to be first?
+// newrelic needs to come before babel to instrument express and pg.
 require('newrelic');
+// babel needs to come before requiring files that use ES6
 require('./babel');
+var throng = require('throng');
 
-// Actually start the server.
-require('./server/init');
+// Concurrency based on https://devcenter.heroku.com/articles/node-concurrency
+console.log("starting with WEB_MEMORY", process.env.WEB_MEMORY, "WEB_CONCURRENCY", process.env.WEB_CONCURRENCY, "MEMORY_AVAILABLE", process.env.MEMORY_AVAILABLE);
+
+var WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+function start() {
+  console.log('Started worker');
+  // Initialize Express
+  require('./server/init');
+  process.on('SIGTERM', function() {
+    console.log('Worker exiting');
+    process.exit();
+  });
+}
+
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity,
+  grace: 4000
+});
